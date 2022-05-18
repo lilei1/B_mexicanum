3e8e13ff2228fb153cfa6e3b0742b6e57460810b
lilei1 <llei@umn.edu>
2022-05-17 21:26:21 -0700

    Add more dir

---
+++ CDS_intron_count/pick_repr_transcript.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+##by Li Lei, 2022-03-22, Alameda;
+#this is to pick the representative transcript from the file.
+
+#usage: 
+use strict;
+use warnings;
+use Data::Dumper;
+my $file= $ARGV[0];
+
+my %tehash;
+
+
+open(SNPID,  "$file") or die "Could not open $file";
+
+foreach my $row (<SNPID>){
+        chomp $row;
+        my @rtemp = split(/\t/,$row);
+        my @tmp = split(/;/,$rtemp[8]);
+        #print "$tmp[0]\n";
+        my @tt =split(/\./,$tmp[1]);
+        #print "$tt[1]\n";
+        if ($tt[2] == 1){
+           print "$row\n";
+        }
+}
+close (SNPID);
